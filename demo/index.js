a152c766b87425626b0c0e4aff882a91b8c6c54f
Yuki Hattori <yuki.hattori@speee.jp>
2019-02-26 21:35:54 +0900

    Add style to demo page

--- demo/index.js
+++ demo/index.js
@@ -11,34 +11,35 @@ registerPlugin('@babel/plugin-transform-react-jsx', babelJsx)
 const jsx = document.getElementById('jsx')
 const json = document.getElementById('json')
 
-jsx.addEventListener(
-  'input',
-  debounce(() => {
-    try {
-      const { code } = transform(jsx.value, {
-        presets: ['es2015'],
-        plugins: [['@babel/plugin-transform-react-jsx', { pragma: 'h' }]],
-      })
+const convert = () => {
+  try {
+    const { code } = transform(jsx.value, {
+      presets: ['es2015'],
+      plugins: [['@babel/plugin-transform-react-jsx', { pragma: 'h' }]],
+    })
 
-      const matched = code.match(/^"use strict";\n\n(h\([\s\S]+\);)$/)
-      if (!matched) throw new Error('Invalid JSX')
+    const matched = code.match(/^"use strict";\n\n(h\([\s\S]+\);)$/)
+    if (!matched) throw new Error('Invalid JSX')
 
-      const funcParams = [
-        'h',
-        ...Object.keys(availableComponents),
-        `"use strict";\nreturn ${matched[1]}`,
-      ]
+    const funcParams = [
+      'h',
+      ...Object.keys(availableComponents),
+      `"use strict";\nreturn ${matched[1]}`,
+    ]
 
-      // eslint-disable-next-line no-new-func
-      const func = new Function(...funcParams)
+    // eslint-disable-next-line no-new-func
+    const func = new Function(...funcParams)
 
-      json.value = JSON.stringify(
-        JSXSlack.JSXSlack(
-          func(JSXSlack.default.h, ...Object.values(availableComponents))
-        )
+    json.value = JSON.stringify(
+      JSXSlack.JSXSlack(
+        func(JSXSlack.default.h, ...Object.values(availableComponents))
       )
-    } catch (e) {
-      console.error(e)
-    }
-  }, 700)
-)
+    )
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+jsx.addEventListener('input', debounce(convert, 700))
+
+convert()

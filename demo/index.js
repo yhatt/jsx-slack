87425dd50a355720cf070aa182bd31925bee6878
Yuki Hattori <yuki.hattori@speee.jp>
2019-02-27 16:06:25 +0900

    Improve editor on demo by using CodeMirror

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,19 @@
 import { registerPlugin, transform } from '@babel/standalone'
 import babelJsx from '@babel/plugin-transform-react-jsx'
+import CodeMirror from 'codemirror'
 import debounce from 'lodash.debounce'
 import * as JSXSlack from '../src/index'
+import example from './example'
+import schema from './schema'
+
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/jsx/jsx'
+import 'codemirror/addon/edit/closebrackets'
+import 'codemirror/addon/edit/closetag'
+import 'codemirror/addon/hint/show-hint'
+import 'codemirror/addon/hint/xml-hint'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/addon/hint/show-hint.css'
 
 const availableComponents = { ...JSXSlack }
 delete availableComponents.default
@@ -10,36 +22,98 @@ registerPlugin('@babel/plugin-transform-react-jsx', babelJsx)
 
 const jsx = document.getElementById('jsx')
 const json = document.getElementById('json')
+const error = document.getElementById('error')
+const errorDetails = document.getElementById('errorDetails')
+const previewBtn = document.getElementById('preview')
+
+json.addEventListener('click', () => json.select())
+
+const completeAfter = (cm, pred) => {
+  if (!pred || pred())
+    setTimeout(() => {
+      if (!cm.state.completionActive) cm.showHint({ completeSingle: false })
+    }, 100)
+  return CodeMirror.Pass
+}
+
+const completeIfAfterLt = cm =>
+  completeAfter(cm, () => {
+    const cur = cm.getCursor()
+    return cm.getRange(CodeMirror.Pos(cur.line, cur.ch - 1), cur) === '<'
+  })
+
+const completeIfInTag = cm =>
+  completeAfter(cm, () => {
+    const tok = cm.getTokenAt(cm.getCursor())
+    if (
+      tok.type === 'string' &&
+      (!/['"]/.test(tok.string.charAt(tok.string.length - 1)) ||
+        tok.string.length === 1)
+    )
+      return false
+    const inner = CodeMirror.innerMode(cm.getMode(), tok.state).state
+    return inner.tagName
+  })
+
+const jsxEditor = CodeMirror(jsx, {
+  autoCloseBrackets: true,
+  autoCloseTags: true,
+  extraKeys: {
+    "'<'": completeAfter,
+    "'/'": completeIfAfterLt,
+    "' '": completeIfInTag,
+    "'='": completeIfInTag,
+    'Ctrl-Space': 'autocomplete',
+  },
+  hintOptions: { schemaInfo: schema },
+  indentUnit: 2,
+  lineWrapping: true,
+  mode: 'jsx',
+  value: example,
+})
 
 const convert = () => {
   try {
-    const { code } = transform(jsx.value, {
+    const { code } = transform(jsxEditor.getValue(), {
       presets: ['es2015'],
       plugins: [['@babel/plugin-transform-react-jsx', { pragma: 'h' }]],
     })
 
-    const matched = code.match(/^"use strict";\n\n(h\([\s\S]+\);)$/)
+    const matched = code.match(/^"use strict";[\s\S]*\n\nh\([\s\S]+\);$/)
     if (!matched) throw new Error('Invalid JSX')
 
-    const funcParams = [
-      'h',
-      ...Object.keys(availableComponents),
-      `"use strict";\nreturn ${matched[1]}`,
-    ]
+    const funcParams = ['h', ...Object.keys(availableComponents), code]
 
     // eslint-disable-next-line no-new-func
     const func = new Function(...funcParams)
 
-    json.value = JSON.stringify(
-      JSXSlack.JSXSlack(
-        func(JSXSlack.default.h, ...Object.values(availableComponents))
-      )
+    let output
+
+    func((...args) => {
+      output = JSXSlack.default.h(...args)
+      return output
+    }, ...Object.values(availableComponents))
+
+    output = JSXSlack.JSXSlack(output)
+
+    json.value = JSON.stringify(output, null, '  ')
+    previewBtn.setAttribute(
+      'href',
+      `https://api.slack.com/tools/block-kit-builder?blocks=${encodeURIComponent(
+        JSON.stringify(output)
+      )}`
     )
+
+    error.classList.add('hide')
   } catch (e) {
+    // eslint-disable-next-line no-console
     console.error(e)
+
+    errorDetails.textContent = e.message.trim()
+    error.classList.remove('hide')
   }
 }
 
-jsx.addEventListener('input', debounce(convert, 700))
+jsxEditor.on('change', debounce(convert, 600))
 
 convert()
---
+++ demo/schema.js
@@ -0,0 +1,182 @@
+const blockCommonAttrs = { id: null, blockId: null }
+const interactiveComponents = [
+  'Button',
+  'Select',
+  'ExternalSelect',
+  'UsersSelect',
+  'ConversationsSelect',
+  'ChannelsSelect',
+  'Overflow',
+  'DatePicker',
+]
+const interactiveCommonAttrs = { actionId: null, confirm: null }
+const markupHTML = [
+  'Escape',
+  'a',
+  'b',
+  'blockquote',
+  'br',
+  'code',
+  'del',
+  'em',
+  'i',
+  'li',
+  'ol',
+  'p',
+  'pre',
+  's',
+  'strike',
+  'strong',
+  'time',
+  'ul',
+]
+
+export default {
+  '!top': ['Block'],
+  Block: {
+    attrs: {},
+    children: [
+      'Section',
+      'Divider',
+      'Image',
+      'Actions',
+      'Context',
+
+      // HTML compatible
+      'section',
+      'hr',
+      'img',
+    ],
+  },
+
+  // Block Kit component
+  Section: {
+    attrs: blockCommonAttrs,
+    children: ['Field', 'Image', ...interactiveComponents, ...markupHTML],
+  },
+  section: {
+    attrs: { id: null },
+    children: ['Field', 'Image', ...interactiveComponents, ...markupHTML],
+  },
+  Field: { attrs: {}, children: markupHTML },
+  Divider: { attrs: blockCommonAttrs, children: [] },
+  hr: { attrs: { id: null }, children: [] },
+  Image: {
+    attrs: { src: null, alt: null, title: null, ...blockCommonAttrs },
+    children: [],
+  },
+  img: {
+    attrs: { src: null, alt: null, title: null, id: null },
+    children: [],
+  },
+  Actions: { attrs: blockCommonAttrs, children: interactiveComponents },
+  Context: { attrs: blockCommonAttrs, children: ['img', ...markupHTML] },
+
+  // Interactive components
+  Button: {
+    attrs: { value: null, url: null, ...interactiveCommonAttrs },
+    children: [],
+  },
+  Select: {
+    attrs: { placeholder: null, value: null, ...interactiveCommonAttrs },
+    children: ['Option', 'Optgroup'],
+  },
+  Option: { attrs: { value: null }, children: [] },
+  Optgroup: {
+    attrs: { label: null },
+    children: ['Option'],
+  },
+  ExternalSelect: {
+    attrs: {
+      placeholder: null,
+      initialOption: null,
+      minQueryLength: null,
+      ...interactiveCommonAttrs,
+    },
+    children: [],
+  },
+  UsersSelect: {
+    attrs: { placeholder: null, initialUser: null, ...interactiveCommonAttrs },
+    children: [],
+  },
+  ConversationsSelect: {
+    attrs: {
+      placeholder: null,
+      initialConversation: null,
+      ...interactiveCommonAttrs,
+    },
+    children: [],
+  },
+  ChannelsSelect: {
+    attrs: {
+      placeholder: null,
+      initialChannel: null,
+      ...interactiveCommonAttrs,
+    },
+    children: [],
+  },
+  Overflow: { attrs: interactiveCommonAttrs, children: ['OverflowItem'] },
+  OverflowItem: { attrs: { value: null, url: null }, children: [] },
+  DatePicker: {
+    attrs: { placeholder: null, initialDate: null, ...interactiveCommonAttrs },
+    children: [],
+  },
+
+  // Composition objects
+  Confirm: {
+    attrs: { title: null, confirm: null, deny: null },
+    children: markupHTML,
+  },
+
+  // Helper component
+  Escape: {
+    attrs: {},
+    children: markupHTML,
+  },
+
+  // HTML elements
+  a: { attrs: { href: null }, children: markupHTML.filter(t => t !== 'a') },
+  b: {
+    attrs: {},
+    children: markupHTML.filter(t => t !== 'b' && t !== 'strong'),
+  },
+  blockquote: {
+    attrs: {},
+    children: markupHTML.filter(t => t !== 'blockquote'),
+  },
+  br: { attrs: {}, children: [] },
+  code: { attrs: {}, children: [] },
+  del: {
+    attrs: {},
+    children: markupHTML.filter(
+      t => t !== 's' && t !== 'strike' && t !== 'del'
+    ),
+  },
+  em: { attrs: {}, children: markupHTML.filter(t => t !== 'i' && t !== 'em') },
+  i: { attrs: {}, children: markupHTML.filter(t => t !== 'i' && t !== 'em') },
+  li: {
+    attrs: {},
+    children: markupHTML.filter(t => t !== 'ul' && t !== 'ol' && t !== 'li'),
+  },
+  ol: { attrs: { start: null }, children: ['li'] },
+  p: { attrs: {}, children: markupHTML.filter(t => t !== 'p') },
+  pre: { attrs: {}, children: [] },
+  s: {
+    attrs: {},
+    children: markupHTML.filter(
+      t => t !== 's' && t !== 'strike' && t !== 'del'
+    ),
+  },
+  strike: {
+    attrs: {},
+    children: markupHTML.filter(
+      t => t !== 's' && t !== 'strike' && t !== 'del'
+    ),
+  },
+  strong: {
+    attrs: {},
+    children: markupHTML.filter(t => t !== 'b' && t !== 'strong'),
+  },
+  time: { attrs: { datetime: null, fallback: null }, children: [] },
+  ul: { attrs: {}, children: ['li'] },
+}

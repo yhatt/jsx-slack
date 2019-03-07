e0ed24b47a0e2977e44beee08e3848c98494b084
Yuki Hattori <yukihattori1116@gmail.com>
2019-03-07 15:36:00 +0900

    Refactor jsxslack tag: Split a bound internal tag

--- src/tag.ts
+++ src/tag.ts
@@ -2,19 +2,20 @@ import { bind } from 'htm'
 import * as components from './components'
 import { JSXSlack } from './index'
 
+const parse = bind((type, props, ...children) => {
+  let elm = type
+
+  // Support built-in components without import
+  if (
+    typeof type === 'string' &&
+    Object.prototype.hasOwnProperty.call(components, type)
+  ) {
+    elm = components[type]
+  }
+
+  return JSXSlack.h(elm, props, ...children)
+})
+
 export default function jsxslack(...args) {
-  return JSXSlack(
-    bind((type, props, ...children) => {
-      let elm = type
-
-      if (
-        typeof type === 'string' &&
-        Object.prototype.hasOwnProperty.call(components, type)
-      ) {
-        elm = components[type]
-      }
-
-      return JSXSlack.h(elm, props, ...children)
-    })(...args)
-  )
+  return JSXSlack(parse(...args))
 }

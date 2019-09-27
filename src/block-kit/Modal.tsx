59d751caa72f1619f7ed5392171722953a6dc324
Yuki Hattori <yuki.hattori@speee.jp>
2019-09-27 20:15:15 +0900

    Add optional attributes for Modal component

--- src/block-kit/Modal.tsx
+++ src/block-kit/Modal.tsx
@@ -5,15 +5,42 @@ import { ObjectOutput } from '../utils'
 import { Blocks, BlockComponentProps } from './Blocks'
 import { plainText } from './composition/utils'
 
+// TODO: Use original View type when supported fields for API on @slack/types
+type ViewForAPI = View & {
+  callback_id?: string
+  external_id?: string
+  hash?: string
+}
+
 export interface ModalProps {
+  callbackId?: string
   children: JSXSlack.Children<BlockComponentProps>
+  clearOnClose?: boolean
+  close?: string
+  externalId?: string
+  hash?: string
+  notifyOnClose?: boolean
+  privateMetadata?: string
+  submit?: string
   title: string
 }
 
 export const Modal: JSXSlack.FC<ModalProps> = props => (
-  <ObjectOutput<View>
-    type="modal"
-    title={plainText(props.title)}
+  <ObjectOutput<ViewForAPI>
     blocks={JSXSlack(<Blocks children={props.children} />)}
+    callback_id={props.callbackId}
+    clear_on_close={
+      props.clearOnClose !== undefined ? props.clearOnClose : undefined
+    }
+    close={props.close ? plainText(props.close) : undefined}
+    external_id={props.externalId}
+    hash={props.hash}
+    notify_on_close={
+      props.notifyOnClose !== undefined ? props.notifyOnClose : undefined
+    }
+    private_metadata={props.privateMetadata}
+    submit={props.submit ? plainText(props.submit) : undefined}
+    title={plainText(props.title)}
+    type="modal"
   />
 )

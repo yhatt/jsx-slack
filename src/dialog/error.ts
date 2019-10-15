794292ffd3db9d41edd3fbad89b4e7829cfbbd67
Yuki Hattori <yuki.hattori@speee.jp>
2019-10-15 09:13:35 +0900

    Update JSDoc for deprecated dialog components

--- src/dialog/error.ts
+++ src/dialog/error.ts
@@ -1,3 +1,4 @@
+/** @deprecated Classic dialog support was deprecated in favor of Slack Modals and will remove in v1. Modal's constraint is not so much stricter as dialog so we don't provide an error object for the modal now. */
 export class DialogValidationError extends Error {
   public constructor(message?: string) {
     super(message)

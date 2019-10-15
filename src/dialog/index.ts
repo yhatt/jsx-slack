c8fdb20404849344b9f6c2dd00aa742512c87833
Yuki Hattori <yuki.hattori@speee.jp>
2019-10-15 09:18:25 +0900

    Output deprecation warning if dialog entry point has loaded

--- src/dialog/index.ts
+++ src/dialog/index.ts
@@ -1,2 +1,6 @@
 export * from './components'
 export { DialogValidationError } from './error'
+
+console.warn(
+  '[DEPRECATION WARNING] Classic dialog support was deprecated in favor of Slack Modals and will remove in v1. Please migrate into Modal provided by the main entry point.'
+)

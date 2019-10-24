fa02622e0bec96f567ecd707518e9c2919a1ca58
Yuki Hattori <yuki.hattori@speee.jp>
2019-10-24 15:51:07 +0900

    Remove unnecessary jsxOnParsed internal hook

--- test/index.tsx
+++ test/index.tsx
@@ -1,24 +1,9 @@
 /** @jsx JSXSlack.h */
-import JSXSlack, { Option, SelectFragment } from '../src/index'
-import { jsxOnParsed } from '../src/jsx'
+import JSXSlack from '../src/index'
 
 beforeEach(() => JSXSlack.exactMode(false))
 
 describe('#JSXSlack', () => {
-  it('executes specified func after parsing if passed internal node has jsxOnParsed symbol prop', () => {
-    const onParsed = jest.fn()
-    const node = (
-      <SelectFragment>
-        <Option value="a">A</Option>
-      </SelectFragment>
-    )
-
-    node.props[jsxOnParsed] = onParsed
-    JSXSlack(node)
-
-    expect(onParsed).toBeCalledTimes(1)
-  })
-
   it('throws error by passed invalid node', () =>
     expect(() => JSXSlack({ props: {}, type: -1 } as any)).toThrow())
 })

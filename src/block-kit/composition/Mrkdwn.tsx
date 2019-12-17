9bc1cd3d253838d92eef998f7f0ef67793e66063
Phil <phil@subcurrent.com>
2019-12-17 12:58:55 -0800

    remove unused function

--- src/block-kit/composition/Mrkdwn.tsx
+++ src/block-kit/composition/Mrkdwn.tsx
@@ -3,7 +3,6 @@ import { JSXSlack } from '../../jsx'
 import { ObjectOutput } from '../../utils'
 import html from '../../html'
 import { BlockComponentProps } from '../Blocks'
-import { MrkdwnElement } from '@slack/types'
 
 interface MrkdwnProps extends BlockComponentProps {
   children: JSXSlack.Children<BlockComponentProps>
@@ -27,16 +26,6 @@ export const Mrkdwn: MrkdwnComponent = ({ children, verbatim }) => {
   return (
     <ObjectOutput<MrkdwnComponentProps>
       type="mrkdwn_component"
-      text={html(children)}
-      verbatim={verbatim}
-    />
-  )
-}
-
-export const mrkdwnToTextObject = ({ children, verbatim }) => {
-  return (
-    <ObjectOutput<MrkdwnElement>
-      type="mrkdwn"
       text={html(children)}
       verbatim={verbatim}
     />

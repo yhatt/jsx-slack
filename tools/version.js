a9057c0c77f92d678b988c985f675f241596c28f
Yuki Hattori <yuki.hattori@speee.jp>
2020-03-03 18:04:57 +0900

    Fix path of CHANGELOG.md for version bump script

--- tools/version.js
+++ tools/version.js
@@ -5,7 +5,7 @@ const unreleased = '## [Unreleased]'
 const [date] = new Date().toISOString().split('T')
 const version = `## v${process.env.npm_package_version} - ${date}`
 
-const changelog = path.resolve(__dirname, 'CHANGELOG.md')
+const changelog = path.resolve(__dirname, '../CHANGELOG.md')
 const content = fs.readFileSync(changelog, 'utf8')
 
 fs.writeFileSync(

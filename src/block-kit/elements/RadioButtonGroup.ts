72426eb9505681c10da32d5546b18f0b8a7bdee3
Yuki Hattori <yuki.hattori@speee.jp>
2020-04-14 13:10:12 +0900

    Add JSDoc for <RadioButtonGroup>

--- src/block-kit/elements/RadioButtonGroup.ts
+++ src/block-kit/elements/RadioButtonGroup.ts
@@ -18,11 +18,63 @@ interface RadioButtons
 
 interface RadioButtonGroupBaseProps extends ActionProps, ConfirmableProps {
   children: JSXSlack.ChildNodes
+
+  /**
+   * A value of the initially selected radio button.
+   *
+   * It must choose a string of `value` prop from defined `<RadioButton>`
+   * elements in children. If not defined, an inital checked button will follow
+   * the state of `<RadioButton checked>`.
+   */
   value?: string | null
 }
 
 type RadioButtonGroupProps = InputComponentProps<RadioButtonGroupBaseProps>
 
+/**
+ * The interactive component or input component for
+ * {@link https://api.slack.com/reference/block-kit/block-elements#radio the `radio_buttons` block element}.
+ *
+ * Provide the container to choose one of the options supplied by
+ * `<RadioButton>`.
+ *
+ * _This component is available only in `<Modal>` and `<Home>` container, and
+ * cannot use in `<Blocks>` container for messaging._
+ *
+ * @example
+ * ```jsx
+ * <Home>
+ *   <Section>
+ *     Select the tier of our service:
+ *     <RadioButtonGroup actionId="tier">
+ *       <RadioButton value="free" checked>
+ *         <b>Free</b>
+ *         <small>$0!</small>
+ *       </RadioButton>
+ *       <RadioButton value="standard">
+ *         <b>Standard</b>
+ *         <small>
+ *           $5/month, <b>and 30 days trial!</b>
+ *         </small>
+ *       </RadioButton>
+ *       <RadioButton value="premium">
+ *         <b>Premium</b>
+ *         <small>$30/month</small>
+ *       </RadioButton>
+ *       <RadioButton value="business">
+ *         <b>Business</b>
+ *         <small>
+ *           <i>Please contact to support.</i>
+ *         </small>
+ *       </RadioButton>
+ *     </RadioButtonGroup>
+ *   </Section>
+ * </Home>
+ * ```
+ *
+ * @return The partial JSON of a block element for the container of radio
+ *   buttons, or `input` layout block with it
+ */
 export const RadioButtonGroup: BuiltInComponent<RadioButtonGroupProps> = createComponent<
   RadioButtonGroupProps,
   RadioButtons | InputBlock

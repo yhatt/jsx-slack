192ea8763fdc52e0dd9058bd0ba16682b7696e73
Yuki Hattori <yuki.hattori@speee.jp>
2020-04-14 12:48:47 +0900

    Add JSDoc for <RadioButton>

--- src/block-kit/elements/RadioButton.ts
+++ src/block-kit/elements/RadioButton.ts
@@ -13,11 +13,64 @@ export interface RadioButtonOption {
 
 export interface RadioButtonProps {
   children: JSXSlack.ChildElements
+
+  /**
+   * A boolean value to indicate an initially checked state of radio button.
+   *
+   * `<RadioButtonGroup>` component may reflect this state.
+   */
   checked?: boolean
+
+  /**
+   * A string or JSX element for the description label of the radio button.
+   *
+   * It accepts formatting through HTML-like elements as same as the content.
+   * The description label can see with faded color just below the main label.
+   */
   description?: JSXSlack.ChildElements
+
+  /** A string value to send to Slack App when choose the radio button. */
   value: string
 }
 
+/**
+ * Define the radio button for the group container provided by
+ * `<RadioButtonGroup>`.
+ *
+ * You should set the label for the radio button in its children. Both of the
+ * content and `description` accept formatting through HTML-like elements
+ * (but except external links by `<a>` tag: Slack will ignore link format).
+ *
+ * ```jsx
+ * <RadioButton
+ *  value="radio"
+ *  description={
+ *    <Fragment>
+ *      It's a <i>description</i>.
+ *    </Fragment>
+ *  }
+ * >
+ *  <b>Radio button</b>
+ * </RadioButton>
+ * ```
+ *
+ * ### Redirect <small> into description
+ *
+ * `<RadioButton>` accepts using `<small>` element for ergonomic templating, to
+ * redirect the content of `<small>` into `description` when `description` prop
+ * was not defined.
+ *
+ * ```jsx
+ * <RadioButton value="radio">
+ *  <b>Radio button</b>
+ *  <small>
+ *    It's a <i>description</i>
+ *  </small>
+ * </RadioButton>
+ * ```
+ *
+ * @return The JSON of the composition object for option suited to radio button
+ */
 export const RadioButton = createComponent<RadioButtonProps, RadioButtonOption>(
   'RadioButton',
   ({ children, checked, description, value }) => {

877082f110df72b5c349e416ac9977af658c04e2
Yuki Hattori <yukihattori1116@gmail.com>
2021-06-14 15:16:47 +0900

    Add useful type exports that are similar to `@types/react`

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,10 @@ export { JSXSlack }
 export * from './components'
 export { jsxslack } from './tag'
 export default JSXSlack
+
+// Useful types similar to @types/react
+export type Node = JSXSlack.ChildElements
+export type FunctionComponent = JSXSlack.FunctionComponent
+export type FC = JSXSlack.FC
+export type VoidFunctionComponent = JSXSlack.VoidFunctionComponent
+export type VFC = JSXSlack.VFC
